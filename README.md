d665528729f14d166643a6894b4222b536b37c1e
sungly <sungly.77@gmail.com>
2018-09-15 18:24:41 -0400

    added initial landing page

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 ## Joyfix
-
+NOTE: To run this project locally, you will need to have node installed on your local machine. 
 
 ### 3rd-party libraries:
     1. react-router 
     2. react-router-dom
-    3. styled-components 
+    3. styled-components
+
+### To run the project locally 
+    - open terminal and go to this project directory
+    - run `npm install` 
+    - run `npm start` 
     
--- src/app/home/index.js
+++ src/app/home/index.js
@@ -3,9 +3,34 @@ import styled from 'styled-components';
 
 import NavBar from '../nav'
 import { PageContainer } from '../shared/PageContainer'
+import LandingImage from '../images/landing_image.png'
 
 const Height = styled.div`
   height: 800px; 
+`;
+
+const LandingContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const LeftContainer = styled.div`
+  min-width: 700px;
+`;
+
+const Title = styled.p`
+  font-weight: bold;
+  font-size: 60px;
+`;
+
+const Subtitle = styled.p`
+  font-size: 30px;
 `;
 
 class Home extends Component {
@@ -13,7 +38,13 @@ class Home extends Component {
       return (
         <PageContainer>
           <NavBar />
-          <p>Home</p>
+          <LandingContainer>
+            <LeftContainer>
+              <Title>Joyfix</Title>
+              <Subtitle>Fixing cars made easy!</Subtitle>
+            </LeftContainer>
+            <Image src={LandingImage} alt="Landing"/>
+          </LandingContainer>
           <Height />
         </PageContainer>
       );
