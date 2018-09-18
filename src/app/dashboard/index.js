875b047fc6517a0e0e91710bbe0a40199202ddc8
sungly <sungly.77@gmail.com>
2018-09-17 21:45:29 -0400

    file format with prettier

--- src/app/dashboard/index.js
+++ src/app/dashboard/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+
+import { PageContainer } from '../shared/PageContainer';
 
 class Dashboard extends Component {
-    render() {
-      return (
-        <p>Dashboard</p>
-      );
-    }
+  render() {
+    return <p>Dashboard</p>;
   }
+}
 
 export default Dashboard;
