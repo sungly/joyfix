8793d3cce6a4c19d37dafeb86d13d49af36bc30c
sungly <sungly.77@gmail.com>
2018-09-19 11:02:40 -0400

    progress bar

--- src/app/dashboard/index.js
+++ src/app/dashboard/index.js
@@ -7,11 +7,6 @@ const TabContainer = styled.div`
   display: flex;
 `;
 
-const Tab = styled.div`
-  width: 100px;
-  border: 1px solid #000;
-`;
-
 const Button = styled.div`
   background-color: ${props => {
     return props.active ? '#1fa3ec' : '	#D3D3D3';
@@ -45,13 +40,77 @@ const Button = styled.div`
   }
 `;
 
+const ProgressContainer = styled.div`
+  width: 100%;
+`;
+
+const ProgressBar = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+
+  counter-reset: step;
+`;
+
+const Step = styled.li`
+  flex-basis: 300px;
+  width: 20%;
+  position: relative;
+  text-align: center;
+
+  &::before {
+    content: counter(step);
+    counter-increment: step;
+    width: 30px;
+    height: 30px;
+    border: 2px solid #bebebe;
+    display: block;
+    margin: 0 auto 10px auto;
+    border-radius: 50%;
+    line-height: 27px;
+    text-align: center;
+    font-weight: bold;
+
+    border-color: ${props => {
+      return props.active ? '#3aac5d' : '';
+    }};
+
+    background: ${props => {
+      return props.active ? '#3aac5d' : 'white';
+    }};
+
+    color: ${props => {
+      return props.active ? 'white' : '#bebebe';
+    }};
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    top: 15px;
+    left: -50%;
+    z-index: -1;
+
+    background: ${props => {
+      return props.active ? '#3aac5d' : '#979797';
+    }};
+  }
+
+  &:first-child:after {
+    content: none;
+  }
+`;
+
 class Dashboard extends Component {
   state = {
-    active: 'appointment'
+    active_tab: 'appointment',
+    completed_step: 3
   };
 
   handleTabClick = type => {
-    this.setState({ active: type });
+    this.setState({ active_tab: type });
   };
 
   render() {
@@ -59,18 +118,25 @@ class Dashboard extends Component {
       <PageContainer>
         <TabContainer>
           <Button
-            active={this.state.active === 'appointment'}
+            active={this.state.active_tab === 'appointment'}
             onClick={() => this.handleTabClick('appointment')}
           >
             Book appointment
           </Button>
           <Button
-            active={this.state.active === 'history'}
+            active={this.state.active_tab === 'history'}
             onClick={() => this.handleTabClick('history')}
           >
             History
           </Button>
         </TabContainer>
+        <ProgressContainer>
+          <ProgressBar>
+            <Step active={this.state.completed_step >= 1}>Select a service</Step>
+            <Step active={this.state.completed_step >= 2}>Fill out information</Step>
+            <Step active={this.state.completed_step >= 3}>Done</Step>
+          </ProgressBar>
+        </ProgressContainer>
       </PageContainer>
     );
   }
